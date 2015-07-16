620bda37ab07e2055373b2cc8b559e8d0a759446
Kevin Emigh <kevin.emigh@ohiohealth.com>
2015-07-16 09:52:03 -0400

    Initial Commit

---
+++ WitTut/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WitTut
+//
+//  Created by Kevin Emigh on 7/16/15.
+//  Copyright (c) 2015 Kevin Emigh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WitTut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WitTut
+//
+//  Created by Kevin Emigh on 7/16/15.
+//  Copyright (c) 2015 Kevin Emigh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
